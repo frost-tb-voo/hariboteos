3173b289091cb4b07b47d0e65a620b5027bb6109
Novs Yama <frost-tb-voo@users.noreply.github.com>
2020-09-09 05:04:07 +0900

    Move the script in chmod_all to nkf_to_utf8.

--- nkf_to_utf8.sh
+++ nkf_to_utf8.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-find . -type f -print0 | xargs -0 nkf -u --overwrite -w
-#find . -type d -exec cp harib00a/Makefile {} \;
+find . -type d -exec chmod 755 {} +
+find . -type f -exec chmod 644 {} +
 
+find . -type d -name "harib*" -exec bash -c "rm {}/*.bat" \;
+find . -type f -print0 | xargs -0 nkf -u --overwrite -w
+
+chmod +x *.sh
