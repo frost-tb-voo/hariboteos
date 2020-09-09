8177ccf5a2656e316da55217f0486d8f1c86ff45
Novs Yama <frost-tb-voo@users.noreply.github.com>
2020-09-10 02:50:22 +0900

    Modify the file conversion script from win to linux using find and nkf commands.

--- nkf_to_utf8.sh
+++ nkf_to_utf8.sh
@@ -2,8 +2,9 @@
 
 find . -type d -exec chmod 755 {} +
 find . -type f -exec chmod 644 {} +
+find . -type f -name "*.sh" -exec chmod +x {} +
 
-find . -type d -name "harib*" -exec bash -c "rm {}/*.bat" \;
+find . -type d -name "harib*" -exec bash -c "rm -f {}/*.bat" \;
 find . -type f -print0 | xargs -0 nkf -u --overwrite -w
+find . -type f -name "*.nkftmp*" -exec rm {} +
 
-chmod +x *.sh
