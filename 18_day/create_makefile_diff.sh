72bf3b48b2d926a3c96d99c3a9a66ffa15e43b5c
Novs Yama <frost-tb-voo@users.noreply.github.com>
2021-01-12 07:06:00 +0900

    Add source files for 18_day.

---
+++ 18_day/create_makefile_diff.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+LAST_DAY=17
+LAST_CODE=14g
+TODAY=18
+NEXT_CODE=15a
+
+# diff between 2 directories
+# bash -c "find . -name 'Makefile' -exec diff -u ../${LAST_DAY}_day/harib${LAST_CODE}/Makefile {} \;" > Makefile_${LAST_DAY}.diff
+# diff in this directory
+# bash -c "find . -name 'Makefile' -exec diff -u ./harib${NEXT_CODE}/Makefile {} \;" > Makefile_${NEXT_CODE}.diff
+
+echo overwrite all
+echo 'cp ../'${LAST_DAY}'_day/harib'${LAST_CODE}'/Makefile ./harib'${NEXT_CODE}'/'
+echo 'find . -type d -name "harib*" -exec cp ./harib'${NEXT_CODE}'/Makefile {} \;'
+
+echo confirm diff from root
+echo 'diff -u ./'${LAST_DAY}'_day/harib'${LAST_CODE}' ./'${TODAY}'_day/harib'${NEXT_CODE}' > ./'${TODAY}'_day/harib'${LAST_CODE}'_'${NEXT_CODE}'.diff'
+echo 'cat ./'${TODAY}'_day/harib'${LAST_CODE}'_'${NEXT_CODE}'.diff | grep "+++"'
+
