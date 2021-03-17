62f6dedfefb90a1f383700b42f092563de2aff82
Novs Yama <frost-tb-voo@users.noreply.github.com>
2021-03-18 08:28:39 +0900

    Add source files for day 26.

---
+++ 26_day/create_makefile_diff.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# diff for Makefile
+LAST_DAY=25
+LAST_CODE=22j
+NEXT_CODE=23a
+
+# diff between 2 directories
+bash -c "find . -name 'Makefile' -exec diff -u ../${LAST_DAY}_day/harib${LAST_CODE}/Makefile {} \;" > Makefile_${LAST_DAY}.diff
+# diff in this directory
+bash -c "find . -name 'Makefile' -exec diff -u ./harib${NEXT_CODE}/Makefile {} \;" > Makefile_${NEXT_CODE}.diff
+
+# overwrite all Makefiles
+echo 'cp ../'${LAST_DAY}'_day/harib'${LAST_CODE}'/Makefile ./harib'${NEXT_CODE}'/'
+echo 'find . -type d -name "harib*" -exec cp ./harib'${NEXT_CODE}'/Makefile {} \;'
+
+
+# diff for all
+LAST_DAY=25
+LAST_CODE=22j
+LAST_DAY=26
+LAST_CODE=23i
+TODAY=26
+NEXT_CODE=23j
+
+# echo diff from root
+# echo 'diff -u ./'${LAST_DAY}'_day/harib'${LAST_CODE}' ./'${TODAY}'_day/harib'${NEXT_CODE}' > ./'${TODAY}'_day/harib'${LAST_CODE}'_'${NEXT_CODE}'.diff'
+# echo 'cat ./'${TODAY}'_day/harib'${LAST_CODE}'_'${NEXT_CODE}'.diff | grep "+++"'
+# echo 'cat ./'${TODAY}'_day/harib'${LAST_CODE}'_'${NEXT_CODE}'.diff | grep "のみに存在"'
